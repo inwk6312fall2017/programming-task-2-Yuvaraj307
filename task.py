037cfe0ea31406378469c21b549d88c3d460592a
Library User <lib-user@TCDA8.WPA.Dal.Ca>
2017-10-02 16:16:12 -0300

    a

--- task.py
+++ task.py
@@ -1,15 +1,10 @@
 import string # learn more: https://python.org/pypi/string
 import csv
 import pandas as pd
-with open('Crime.csv','r') as myfile:
-  r = csv.reader(myfile)
-  rows = list(r)
-  print("")
-  
-  
 df = pd.read_csv('Crime.csv')
 list(set(df.RUCR_EXT_D))
+df.head(9)
 g = list(df['RUCR_EXT_D'].unique())
-g = df.groupby('RUCR_EXT_D')
-g['RUCR_EXT_D'].nunique()
-print(list(g))
+gi = df.groupby('RUCR_EXT_D')
+oi = df.groupby('RUCR')
+print(gi['RUCR_EXT_D'].nunique(), oi['RUCR'].nunique())
