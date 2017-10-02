6aee1293a427ea6ca463450ca915ff08496d4d2b
labstudent <yuvaraj@dal.ca>
2017-10-02 12:47:50 -0300

    today

---
+++ task.py
@@ -0,0 +1,39 @@
+import csv
+
+with open('Crime.csv','r') as myfile:
+
+reader = csv.reader(myfile)
+
+rows = list(reader)
+
+import csv
+
+fin = open('Crime.csv')
+
+words = ["Assault","Robbery","BREAK AND ENTER","THEFT FROM VEHICLE"]
+
+found = {}
+
+count = 0
+
+for line in fin:
+
+  for word in words:
+
+      if word in line:
+
+          count = count + 1
+
+  found[word] = count
+
+print(found)
+
+
+
+df = pd.read_csv('Crime.csv')
+
+list(set(df.Description))
+
+g = list(df['Description'].unique())
+
+print(g)
